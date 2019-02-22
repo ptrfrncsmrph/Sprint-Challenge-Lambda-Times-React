4004fa37b94b28de4cdc1c8517cac8c2d8356e8c
Peter Murphy <peter.francis.murphy@gmail.com>
2019-02-22 12:47:26 -0500

    Fixes typo

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -16,9 +16,7 @@ const StyledHeader = styled.div`
   margin-top: 44px;
   padding-bottom: 15px;
   @media (min-width: 1280px) {
-     {
-      width: 1280px;
-    }
+    width: 1280px;
   }
   h1 {
     font-size: 60px;
