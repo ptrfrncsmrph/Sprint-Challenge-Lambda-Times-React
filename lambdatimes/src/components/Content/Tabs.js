e8b0445df8db0821990a7e9816455367b3bd18b3
Peter Murphy <peter.francis.murphy@gmail.com>
2019-02-22 11:47:30 -0500

    Builds out Content logic, passes props to Tabs component

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,15 +1,30 @@
 import React from "react"
 import Tab from "./Tab"
-const Tabs = props => {
+import PropTypes from "prop-types"
+
+const Tabs = ({ tabs, selectedTab, selectedTabHandler }) => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        {tabs.map(tab => (
+          <Tab
+            tab={tab}
+            selectedTab={selectedTab}
+            selectedTabHandler={selectedTabHandler}
+          />
+        ))}
       </div>
     </div>
   )
+}
+
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.string).isRequired,
+  selectedTab: PropTypes.string.isRequired,
+  selectedTabHandler: PropTypes.func.isRequired
 }
 
 // Make sure to use PropTypes to validate your types!
